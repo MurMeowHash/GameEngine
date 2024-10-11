d50dc77a03a868672283b23cff72fe10f6a9aa08
Oleksandr Havrysh <gavrishalex95@gmail.com>
2024-10-11 19:31:19 +0300

    Application class as driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "Application/Application.h"
 
 int main() {
-    Application::create(true, "Drone Game");
+    Application::create(false, "Drone Game");
     Application::run();
     Application::terminate();
     return 0;
