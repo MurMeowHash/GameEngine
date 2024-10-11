05af50d301d40662b218e1da5ccc2f31eab39317
Oleksandr Havrysh <gavrishalex95@gmail.com>
2024-10-11 19:33:13 +0300

    Default material was provided

--- src/ManagersSystem/ResourceManager.cpp
+++ src/ManagersSystem/ResourceManager.cpp
@@ -1,16 +1,30 @@
 #include "ResourceManager.h"
 #include "../Debug/Debug.h"
 
+ResourceManager::ResourceManager() : defaultTexture{nullptr}, defaultMaterial{nullptr} {
+
+}
+
 void ResourceManager::startUp() {
     logManagerLoading("Resource");
     defaultTexturePath = "../global_textures/default_texture.png";
     defaultTexture = new Texture2D{defaultTexturePath};
     loadedTextures.emplace(defaultTexturePath, defaultTexture);
+    defaultMaterial = new ColorMaterial;
+    if(!*defaultMaterial) {
+        state = ManagerState::Incomplete;
+        Debug::logError("MATERIAL", "DEFAULT_MATERIAL_CAN_NOT_BE_LOADED");
+        return;
+    }
     state = ManagerState::Started;
 }
 
 Texture2D *ResourceManager::getDefaultTexture() const {
     return defaultTexture;
+}
+
+ColorMaterial *ResourceManager::getDefaultMaterial() const {
+    return defaultMaterial;
 }
 
 Texture2D *ResourceManager::findTexture(const char *path) {
@@ -30,4 +44,5 @@ void ResourceManager::dispose() {
         loadedTexture.second->dispose();
     }
     delete defaultTexture;
+    delete defaultMaterial;
 }
--- src/ManagersSystem/ResourceManager.h
+++ src/ManagersSystem/ResourceManager.h
@@ -2,6 +2,7 @@
 
 #include "AbstractManager.h"
 #include "../Game/Texture/Texture2D.h"
+#include "../Game/Material/ColorMaterial.h"
 #include "../utils/utils.h"
 #include <unordered_map>
 #include "../Dispose/IDisposable.h"
@@ -10,10 +11,13 @@ class ResourceManager : public AbstractManager, public IDisposable {
 private:
     const char *defaultTexturePath;
     Texture2D *defaultTexture;
+    ColorMaterial *defaultMaterial;
     std::unordered_map<const char *, Texture2D *> loadedTextures;
 public:
+    explicit ResourceManager();
     void startUp() override;
     Texture2D *getDefaultTexture() const;
+    ColorMaterial *getDefaultMaterial() const;
     Texture2D *findTexture(const char *path);
     void addTexture(const char *path, Texture2D *texture);
     void dispose() override;
