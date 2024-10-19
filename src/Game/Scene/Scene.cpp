9c7748e1d72783ce133c6b3acc5d744a179a2fa2
Oleksandr Havrysh <gavrishalex95@gmail.com>
2024-10-19 19:49:31 +0300

    Scene got name and currently active camera

--- src/Game/Scene/Scene.cpp
+++ src/Game/Scene/Scene.cpp
@@ -1,1 +1,13 @@
 #include "Scene.h"
+
+void Scene::applyLight(Shader *shader) {
+
+}
+
+const char *Scene::getName() const {
+    return name;
+}
+
+Camera *Scene::getActiveCamera() const {
+    return activeCamera;
+}
--- src/Game/Scene/Scene.h
+++ src/Game/Scene/Scene.h
@@ -1,5 +1,15 @@
 #pragma once
 
-class Scene {
+#include "../../utils/utils.h"
+#include "../Camera/Camera.h"
+#include "../../Shader/Shader.h"
 
+class Scene {
+private:
+    const char *name;
+    Camera *activeCamera;
+public:
+    void applyLight(Shader *shader);
+    NODISCARD const char* getName() const;
+    NODISCARD Camera* getActiveCamera() const;
 };
