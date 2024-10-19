ebe61db5e3995d58c4e793481f9b6cc9c6ed49e1
Oleksandr Havrysh <gavrishalex95@gmail.com>
2024-10-19 19:50:22 +0300

    Spotlight features were added

--- src/Game/Light/SpotLight.cpp
+++ src/Game/Light/SpotLight.cpp
@@ -1,1 +1,28 @@
 #include "SpotLight.h"
+
+SpotLight::SpotLight(GLfloat innerCutOff, GLfloat outerCutOff, GLfloat radius, const glm::vec3 &ambient,
+                     const glm::vec3 &diffuse, const glm::vec3 &specular, const glm::vec3 &position,
+                     const glm::vec3 &rotation)
+                     : PointLight("spotLights", currentIndex, radius, ambient, diffuse, specular, position, rotation) {
+    setInnerCutOff(innerCutOff);
+    setOuterCutOff(outerCutOff);
+}
+
+void SpotLight::setInnerCutOff(GLfloat cutOff) {
+    cutOff = glm::clamp(cutOff, MIN_CUT_OFF, MAX_CUT_OFF);
+    innerCutOff = glm::radians(cutOff);
+}
+
+void SpotLight::setOuterCutOff(GLfloat cutOff) {
+    cutOff = glm::clamp(cutOff, innerCutOff, MAX_CUT_OFF);
+    outerCutOff = glm::radians(cutOff);
+}
+
+bool SpotLight::set(Shader *shader) {
+    if(PointLight::set(shader)) {
+        shader->setFloat((lightName + ".innerCutOff").c_str(), innerCutOff);
+        shader->setFloat((lightName + ".outerCutOff").c_str(), outerCutOff);
+        return true;
+    }
+    return false;
+}
--- src/Game/Light/SpotLight.h
+++ src/Game/Light/SpotLight.h
@@ -3,5 +3,20 @@
 #include "PointLight.h"
 
 class SpotLight : public PointLight {
-
+private:
+    static constexpr GLfloat DEFAULT_INNER_CUT_OFF{10.0f};
+    static constexpr GLfloat DEFAULT_OUTER_CUT_OFF{15.0f};
+    static constexpr GLfloat MIN_CUT_OFF{0.0f};
+    static constexpr GLfloat MAX_CUT_OFF{180.0f};
+    GLfloat innerCutOff;
+    GLfloat outerCutOff;
+    static GLuint currentIndex;
+public:
+    explicit SpotLight(GLfloat innerCutOff = DEFAULT_INNER_CUT_OFF, GLfloat outerCutOff = DEFAULT_OUTER_CUT_OFF,
+              GLfloat radius = DEFAULT_RADIUS, const glm::vec3 &ambient = DEFAULT_AMBIENT,
+              const glm::vec3 &diffuse = DEFAULT_DIFFUSE, const glm::vec3 &specular = DEFAULT_SPECULAR,
+              const glm::vec3 &position = DEFAULT_POS, const glm::vec3 &rotation = DEFAULT_ROT);
+    void setInnerCutOff(GLfloat cutOff);
+    void setOuterCutOff(GLfloat cutOff);
+    bool set(Shader *shader) override;
 };
